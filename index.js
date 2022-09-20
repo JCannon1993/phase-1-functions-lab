63f2f9e9607fc0153531e457973211491763d89c
Jelani Cannon <jelani.cannon@gmail.com>
2022-09-20 12:37:32 -0700

    commit tree 694dd492ba5f0383b671b7430fbf3a629d45df49

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (blocks){
+    if (blocks > 42){
+        return (blocks-42)
+    }
+    else {
+        return (42 - blocks)
+    }
+}
+
+function distanceFromHqInFeet(street) {
+    distanceFromHqInBlocks(street)
+     let feet = (distanceFromHqInBlocks(street) * 264)
+     return feet
+    }
+
+function distanceTravelledInFeet(start, destination){
+    if (destination > start){
+        return (destination - start)*264
+    }
+    else {
+        return (start - destination)*264
+    }
+}
+function calculatesFarePrice (start, destination){
+ let fare = (distanceTravelledInFeet(start, destination))
+    if (fare < 400){
+        return 0
+    }
+    else if (fare >400 && fare <= 2000){
+        return (fare - 400)* .02
+    }
+    else if (fare > 2000 && fare < 2500){
+        return 25
+    }
+    else if (fare > 2500){
+        return "cannot travel that far"
+    }
+}
